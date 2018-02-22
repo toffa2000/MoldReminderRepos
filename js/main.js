6e30965ffc6d122974009f68718f66c9b9c39582
Mattia Toffanin <mattia.toffanin@gmail.com>
2018-02-22 08:41:15 +0100

    Add chatdiv

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
-function openChat() {
-    $()
+
+$(".chatbutton").click(function() {
+    $(".chatbody").slideUp(400);
+
+    $(".messdiv").delay(400).fadeIn(400);
+    $(".messdiv").delay(2400).fadeOut(2000);
+})
+
+$(".chatmenu").click(function() {
+    $(".chatbody").slideDown(400, function() {
+        $(".chatdiv").slideDown(400);
+    });
+})
+
+$(".closechat").click(function() {
+    $(".chatdiv").slideUp(400, resetValue());
+})
+
+$(".minimize").click(function() {
+    $(".chatbody").slideUp(400);
+})
+
+$(".chatheader p").click(function() {
+    $(".chatbody").slideDown(400);
+})
+
+
+function resetValue() {
+    $(".chatname").val("");
+    $(".chatmessage").val("");
 }
